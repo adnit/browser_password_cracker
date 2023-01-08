720c43dc43cc4813daef1ab0fdb690db77c92dba
adnit <adnitkamberi10@gmail.com>
2023-01-08 20:36:01 +0100

    shows main menu options according to os

--- Main.py
+++ Main.py
@@ -3,24 +3,33 @@ import Chrome
 import Edge
 from PasswordCrawler import PasswordCrawler
 from tabulate import tabulate
+import platform
 
-val = input("You have 3 options: \n"
-            "Chrome\n"
-            "Edge\n"
-            "Mozilla\n"
-            "Chrome-Linux\n"
-            "What browser do you want to use: ")
+chromelinuxStr = "Chrome-Linux"
 
-if __name__ == "__main__":
-    if val == "Edge":
-        Edge.edge()
-    elif val == "Chrome":
-        Chrome.chrome()
-    elif val == "Mozilla":
-        firefox_decrypt.main()
-    elif val == "Chrome-Linux":
+if platform.system() == 'Linux':
+    val = input('You have {} option{}: \n'
+                '{} \n'
+                'What browser do you want to use: '.format("1","", chromelinuxStr))
+    if val == chromelinuxStr:
         chromeLinux = PasswordCrawler()
         data = chromeLinux.dump_passwords()
         print(tabulate(data, headers=["url", "username", "password"]))
     else:
         print("Browser typed wrong or is not supported")
+else:
+    val = input("You have 3 options: \n"
+                "Chrome\n"
+                "Edge\n"
+                "Mozilla\n"
+                "What browser do you want to use: ")
+
+    if __name__ == "__main__":
+        if val == "Edge":
+            Edge.edge()
+        elif val == "Chrome":
+            Chrome.chrome()
+        elif val == "Mozilla":
+            firefox_decrypt.main()
+        else:
+            print("Browser typed wrong or is not supported")
